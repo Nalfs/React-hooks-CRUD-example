17ebd478d746bf42fd1b3945be7b446304078565
Nalfs <nadim.al.sharif@gmail.com>
2019-02-15 12:21:55 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import Usertable from './tables/UserTable'
+import AddUserForm from './forms/AddUserForm'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = () => {
+
+  const usersData = [
+    { id: 1, name: 'Kadar', username: 'kazzak' },
+    { id: 2, name: 'Kaducus', username: 'timermaw' },
+    { id: 3, name: 'ElajtVolvo', username: 'maxmax' },
+  ]
+
+	const [ users,setUsers ] = useState(usersData)
+
+	const addUser = user => {
+		user.id = users.length + 1
+		setUsers([ ...users, user ])
+	}
+
+	const deleteUser = id => {
+		setUsers(users.filter(user => user.id !== id))
+	}
+
+	console.log('my user', users)
+
+	return (
+
+		<div className="container">
+			<h1>CRUD App with Hooks</h1>
+			<div className="flex-row">
+				<div className="flex-large">
+					<h2>Add user</h2>
+					<AddUserForm addUser={addUser} />
+				</div>
+				<div className="flex-large">
+          <h2>View users</h2>
+          <Usertable users={users} deleteUser={deleteUser}/>
+				</div>
+			</div>
+		</div>
+	)
 }
-
 export default App;
